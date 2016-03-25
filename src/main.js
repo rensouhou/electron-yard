cd2f90fcc57a687a940e231164886136a2f213d6
Stefan Rimaila <stefan@fusetools.com>
2016-03-25 19:33:47 +0200

    Basic stuff

--- src/main.js
+++ src/main.js
@@ -1,9 +1,40 @@
 /**
  * @overview
  *
- * @since 0.0.0
- * @author Anon
+ * @since 0.0.1
+ * @author Stefan Rimaila <stefan@rimaila.fi>
  * @module
+ *
+ * @todo Proxy for intercepting traffic
  */
+'use strict';
 
-console.log('o hai');
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+var mainWindow = null;
+
+app.commandLine.appendSwitch('remote-debugging-port', '8609');
+app.commandLine.appendSwitch('host-rules', 'MAP *.dmm.com localhost:8420');
+
+app.on('window-all-closed', function () {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function () {
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  var ses = mainWindow.webContents.session;
+
+  ses.webRequest.onCompleted(function (listener) {
+    console.log(listener);
+  });
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+});
