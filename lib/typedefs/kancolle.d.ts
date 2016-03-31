3d5134bc159a24364a80c6131f749a0946d2536f
Stefan Rimaila <stefan@fusetools.com>
2016-03-31 15:55:07 +0300

    Base KCS typedefs

---
+++ lib/typedefs/kancolle.d.ts
@@ -0,0 +1,23 @@
+/**
+ * Models
+ */
+export namespace KCS {
+    class Ship {
+    }
+
+    class SlotItem {
+    }
+
+    class ConstructionDock {
+    }
+
+    class RepairDock {
+    }
+}
+
+/**
+ * API stuff
+ */
+export namespace KCSApi {
+    
+}
