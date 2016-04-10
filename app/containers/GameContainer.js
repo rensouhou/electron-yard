5112dc5f76f42b86fca87cdf49a3f73e1087cd59
Stefan Rimaila <stefan@fusetools.com>
2016-04-10 09:25:03 +0200

    Removed redundant component

--- app/containers/GameContainer.js
+++ app/containers/GameContainer.js
@@ -5,23 +5,11 @@
  * @author Stefan Rimaila <stefan@rimaila.fi>
  * @module app/containers/Game
  */
-import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { registerGameView } from '../actions/app-core';
 import Game from '../components/Game';
 
-class GameContainer extends Component {
-  static propTypes = {
-    gameURL: PropTypes.any
-  };
-
-  render() {
-    const { gameURL } = this.props;
-    return <Game gameURL={gameURL} />;
-  }
-}
-
 export default connect(
   null,
   { registerGameView }
-)(GameContainer);
+)(Game);
