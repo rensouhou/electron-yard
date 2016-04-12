4eeea849c65b27a638ca02e994cbbb182a288df9
Stefan Rimaila <stefan@fusetools.com>
2016-04-13 00:04:19 +0200

    Added `asBool`

--- app/transformers/primitive.js
+++ app/transformers/primitive.js
@@ -4,8 +4,9 @@
  * @since 0.3.0
  * @author Stefan Rimaila <stefan@rimaila.fi>
  * @module app/transformers/primitive
+ * @flow
  */
 
 export const asNumber = (num:any):number => parseInt(num, 10);
-
+export const asBool = (n:number):boolean => n === 1;
 export const formatLineBreaks = (s:string):string => (s || '').replace(/<br>/gi, '\n');
