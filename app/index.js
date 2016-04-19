e96beb1160bbe09b7efe4f975d8b298e2a35a9be
Stefan Rimaila <stefan@fusetools.com>
2016-04-19 20:33:39 +0200

    Added base stylesheets, nonstandard for now

--- app/index.js
+++ app/index.js
@@ -14,6 +14,7 @@ import { syncHistoryWithStore } from 'react-router-redux';
 import routes from './routes';
 import configureStore from './store/configure-store';
 
+import './assets/css/app.global.css';
 import './components/electron';
 
 const store = configureStore();
