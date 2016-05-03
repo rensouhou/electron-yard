0a179a3f136a8a3dbb3b26421a29cdad0942dc58
Stefan Rimaila <stefan@fusetools.com>
2016-05-03 08:58:10 +0300

    Added RethinkDB middleware for logging purposes

---
+++ src/main/schema/rdb.js
@@ -0,0 +1,45 @@
+/**
+ * @overview
+ *
+ * @since 0.4.0
+ * @author Stefan Rimaila <stefan@rimaila.fi>
+ * @module src/main/schema/rdb
+ */
+import invariant from 'invariant';
+
+export default function (thinky, type) {
+  invariant(thinky && type, 'Thinky instance required');
+
+  const MaterialState = thinky.createModel('MaterialState', {
+    timestamp: type.date().default(new Date()),
+    fuel: type.number().integer(),
+    ammo: type.number().integer(),
+    steel: type.number().integer(),
+    bauxite: type.number().integer(),
+    instantConstruction: type.number().integer().optional(),
+    instantRepair: type.number().integer().optional(),
+    developmentMaterials: type.number().integer().optional(),
+    improvementMaterials: type.number().integer().optional()
+  });
+  MaterialState.ensureIndex('timestamp');
+
+  const CraftingLog = thinky.createModel('CraftingLog', {
+    timestamp: type.date().default(new Date()),
+    fuel: type.number().integer(),
+    ammo: type.number().integer(),
+    steel: type.number().integer(),
+    bauxite: type.number().integer(),
+    flags: {
+      successful: type.boolean().optional(),
+      usedDevelopmentMaterials: type.boolean().optional(),
+      lsc: type.boolean().optional(),
+      instant: type.boolean().optional()
+    },
+    id: type.number().optional(),
+    entityId: type.number().optional()
+  });
+
+  CraftingLog.ensureIndex('timestamp');
+
+  return { MaterialState, CraftingLog };
+}
